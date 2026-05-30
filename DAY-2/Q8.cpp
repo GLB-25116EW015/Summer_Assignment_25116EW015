69f1b6153751b62fa8f4c5f9fa4c782dacf174df
Mahi <mahisinghaks@gmail.com>
2026-05-30 20:32:36 +0530

    Added DAY-2 programs

---
+++ DAY-2/Q8.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, original, reverse = 0;
+
+    cout << "Enter a number: ";
+    cin >> n;
+
+    original = n;
+
+    while (n > 0) {
+        reverse = reverse * 10 + (n % 10);
+        n /= 10;
+    }
+
+    if (original == reverse)
+        cout << "Palindrome Number";
+    else
+        cout << "Not a Palindrome Number";
+
+    return 0;
+}
