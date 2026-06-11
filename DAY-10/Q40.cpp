545367c7c6521aaf8f4ec448bbc36a7d36841461
Mahi <mahisinghaks@gmail.com>
2026-06-11 21:24:32 +0530

    Added DAY-10 programs

---
+++ DAY-10/Q40.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int rows=5;
+    for(int i = 0; i<rows; i++){
+        for(char ch='A'; ch<='A'+i; ch++){
+            cout << ch;
+        }
+        for(char ch='A'+i-1; ch>='A'; ch--){
+            cout << ch;
+        }
+        cout << endl;
+    }return 0;
+}
