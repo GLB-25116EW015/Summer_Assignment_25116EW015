c0d058e54e5a82fb1008d2ac8a57de9fd3e3d128
Mahi <mahisinghaks@gmail.com>
2026-06-21 20:53:33 +0530

    Added DAY-21 programs

---
+++ DAY-21/Q81.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    string str;
+    getline(cin, str);
+    int length=0;
+    for (char ch:str) {
+        length++;
+    }
+    cout<<length;
+    return 0;
+}
---
+++ DAY-21/Q84.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cctype>
+using namespace std;
+int main(){
+    string str;
+    getline(cin,str);
+    for (char &ch:str) {
+        ch = toupper(ch);
+    }
+    cout << str;
+    return 0;
+}
